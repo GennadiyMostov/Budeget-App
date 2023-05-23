8a2609f8a8b63a82e8a3c799dfbead848931d869
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-05-23 12:01:24 -0500

    Added classes to expense list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,18 +10,24 @@ const Expenses = ({ items }) => {
   //Maps Expense Items Data
   const ifExpenseContentTrue = filteredList.map((expenseData) => {
     return (
-      <ExpenseItem
-        key={expenseData.id}
-        title={expenseData.title}
-        amount={expenseData.amount}
-        date={expenseData.date}
-      />
+      <ul className='expenses-list'>
+        <ExpenseItem
+          key={expenseData.id}
+          title={expenseData.title}
+          amount={expenseData.amount}
+          date={expenseData.date}
+        />
+      </ul>
     );
   });
 
+  const ifExpenseContentFalse = (
+    <h2 className='expenses-list__fallback'>No Expenses Found</h2>
+  );
+
   //If No Expense Data, Return 'p' Element, else return data.
   const expenseContent =
-    filteredList.length === 0 ? <p>No Expenses Found</p> : ifExpenseContentTrue;
+    filteredList.length === 0 ? ifExpenseContentFalse : ifExpenseContentTrue;
 
   return (
     <div>
